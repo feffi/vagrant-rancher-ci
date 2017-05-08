87810598cdf603104208e4805b9e2fb22cdb8086
feffi <feffi@feffi.org>
2017-05-08 10:42:29 +0200

    added initial config

---
+++ config.rb
@@ -0,0 +1,37 @@
+# Vagrant box configuration details
+# Image of the box to use
+# $box = "rancherio/rancheros"
+
+# Version of the box image
+# $box_version = '>=0.4.1'
+
+# Official CoreOS channel. Either alpha, beta or stable
+# $update_channel = "alpha"
+
+# URL to pull CoreOS image from
+# $box_url = "https://storage.googleapis.com/%s.release.core-os.net/amd64-usr/current/coreos_production_vagrant.json" % [$update_channel]
+
+# Tag of the rancher/server image to run
+# $rancher_version = 'latest'
+
+# IP prefix to use when assigning box ip addresses
+# $ip_prefix = '10.0.0'
+
+# Enable syncing of the current directory to the /vagrant path on the guest
+# $disable_folder_sync = false
+
+# Boxes to create in the vagrant environment
+$boxes = [
+    {
+      "name"   => "rancher-server",
+      "role"   => "server",
+      "memory" => "1536",
+      "labels" => [],
+    },
+    {
+      "name"   => "rancher-agent",
+      "count"  => 4,
+      "memory" => "512",
+      "labels" => ["type=general"]
+    },
+]
